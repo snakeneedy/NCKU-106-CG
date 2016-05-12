3a321688e2d88261ff780ec3004f371a27ed2564
Pin-Shiuan She <snakeneedy@gmail.com>
2016-05-12 19:13:57 +0800

    Outsource shaders in pr02/

--- pr02/main.cpp
+++ pr02/main.cpp
@@ -6,10 +6,14 @@
 // #include <glm/gtc/matrix_transform.hpp>
 #include <cstdio>
 #include <cstdlib>
+#include <string>
+#include <fstream>
 
 static void error_callback (int error, const char * description);
 static void key_callback (GLFWwindow * window, int key, int scancode, int action, int mods);
 static void render ();
+static std::string readfile (const char *filename);
+static unsigned int setup_shader(const char *vertex_shader, const char *fragment_shader);
 
 GLFWwindow * window;
 GLuint vs, fs, program, vao, vbo;
@@ -53,55 +57,7 @@ int main (int argc, char * argv[])
 	//glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 	// shader
-	//int status, maxLength;
-	//char * infoLog = nullptr;
-	const char* vertex_shader =
-		"#version 400\n"
-		"in vec3 vp;"
-		"void main () {"
-		"  gl_Position = vec4 (vp, 1.0);"
-		"}";
-
-	vs = glCreateShader(GL_VERTEX_SHADER);
-	//glShaderSource(vs, 1, (const GLchar**)("shaders/vs.glsl"), nullptr);
-	glShaderSource(vs, 1, &vertex_shader, nullptr);
-	glCompileShader(vs);
-	//glGetShaderiv(vs, GL_COMPILE_STATUS, &status);
-	//if (status == GL_FALSE)
-	//{
-	//	glGetShaderiv(vs, GL_INFO_LOG_LENGTH, &maxLength);
-	//	infoLog = new char[maxLength];
-	//	glGetShaderInfoLog(vs, maxLength, &maxLength, infoLog);
-	//	fprintf(stderr, "Vertex Shader Error: %s\n", infoLog);
-	//	delete [] infoLog;
-	//	return EXIT_FAILURE;
-	//}
-	const char* fragment_shader =
-		"#version 400\n"
-		"out vec4 frag_colour;"
-		"void main () {"
-		"  frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
-		"}";
-
-	fs = glCreateShader(GL_FRAGMENT_SHADER);
-	//glShaderSource(fs, 1, (const GLchar**)("shaders/fs.glsl"), nullptr);
-	glShaderSource(fs, 1, &fragment_shader, nullptr);
-	glCompileShader(fs);
-	//glGetShaderiv(fs, GL_COMPILE_STATUS, &status);
-	//if (status == GL_FALSE)
-	//{
-	//	glGetShaderiv(fs, GL_INFO_LOG_LENGTH, &maxLength);
-	//	infoLog = new char[maxLength];
-	//	glGetShaderInfoLog(fs, maxLength, &maxLength, infoLog);
-	//	fprintf(stderr, "Vertex Shader Error: %s\n", infoLog);
-	//	delete [] infoLog;
-	//	return EXIT_FAILURE;
-	//}
-	// program
-	program = glCreateProgram();
-	glAttachShader(program, vs);
-	glAttachShader(program, fs);
-	glLinkProgram(program);
+	program = setup_shader(readfile("shaders/vs.glsl").c_str(), readfile("shaders/fs.glsl").c_str());
 	//
 	while( !glfwWindowShouldClose(window) )
 	{
@@ -132,3 +88,69 @@ static void render ()
 	glfwPollEvents();
 	glfwSwapBuffers(window);
 }
+
+static std::string readfile (const char *filename)
+{
+	std::ifstream ifs(filename);
+	if(!ifs)
+		exit(EXIT_FAILURE);
+	return std::string( (std::istreambuf_iterator<char>(ifs)),
+			(std::istreambuf_iterator<char>()));
+}
+
+static unsigned int setup_shader(const char *vertex_shader, const char *fragment_shader)
+{
+	int status, maxLength;
+	char *infoLog=nullptr;
+
+	GLuint vs=glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, (const GLchar**)&vertex_shader, nullptr);
+	glCompileShader(vs);
+
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &status);
+	if(status==GL_FALSE)
+	{
+		glGetShaderiv(vs, GL_INFO_LOG_LENGTH, &maxLength);
+		infoLog = new char[maxLength];
+		glGetShaderInfoLog(vs, maxLength, &maxLength, infoLog);
+		fprintf(stderr, "Vertex Shader Error: %s\n", infoLog);
+		delete [] infoLog;
+		return 0;
+	}
+
+	GLuint fs=glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, (const GLchar**)&fragment_shader, nullptr);
+	glCompileShader(fs);
+
+	glGetShaderiv(fs, GL_COMPILE_STATUS, &status);
+	if(status==GL_FALSE)
+	{
+		glGetShaderiv(fs, GL_INFO_LOG_LENGTH, &maxLength);
+		infoLog = new char[maxLength];
+		glGetShaderInfoLog(fs, maxLength, &maxLength, infoLog);
+		fprintf(stderr, "Fragment Shader Error: %s\n", infoLog);
+		delete [] infoLog;
+		return 0;
+	}
+
+	unsigned int program=glCreateProgram();
+	// Attach our shaders to our program
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+
+	glLinkProgram(program);
+
+	glGetProgramiv(program, GL_LINK_STATUS, &status);
+	if(status==GL_FALSE)
+	{
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+		infoLog = new char[maxLength];
+		glGetProgramInfoLog(program, maxLength, NULL, infoLog);
+		glGetProgramInfoLog(program, maxLength, &maxLength, infoLog);
+		fprintf(stderr, "Link Error: %s\n", infoLog);
+		delete [] infoLog;
+		return 0;
+	}
+	return program;
+}
+
